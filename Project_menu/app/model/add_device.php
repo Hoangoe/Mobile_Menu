d38a6a56d9dd78b58cb1e59eac126886c61e9006
Hoangoe <boyminhhoang2001@gmail.com>
2023-11-28 15:11:02 +0700

    add file

---
+++ Project_menu/app/model/add_device.php
@@ -0,0 +1,21 @@
+<?php
+session_start();
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<div class="add">
+    <form action="submit" method="post">
+    <span href="">Tên sản phẩm: <input type="name"></span>
+    <button type="submit">Sent</button>
+  </form>
+  <?php
+  ?>
+</div>
+</body>
+</html>
